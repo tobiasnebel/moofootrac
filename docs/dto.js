2f6a52cd531695db7215ee51d830d21cfde6ad1d
tobias.nebel@sciota.io <tobias.nebel@sciota.io>
2025-10-05 20:34:06 +0200

    added example dto

---
+++ docs/dto.js
@@ -0,0 +1,9 @@
+{
+    // mandatory:
+    "mood": "Mäh",
+    // optional:
+    "food1": "ne schüssel müsli",
+    "food1Time": "Morgen",
+    "food2": "4 esslöffel nutella (gehäuft!)",
+    "food2Time":"Abend"
+}
