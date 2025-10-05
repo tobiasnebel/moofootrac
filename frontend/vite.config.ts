c8efbdd5fd6656ed90889e86a66877efde264b8a
tobias.nebel@sciota.io <tobias.nebel@sciota.io>
2025-10-06 01:24:50 +0200

    fix path issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api': {
-      //   target: 'http://127.0.0.1:9011',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ''),
-      // }
+      '/api': {
+        target: 'http://127.0.0.1:9011',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      }
     },
   },
   base: "/app/",
